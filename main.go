38ab565ef74832ddb8375b77db8b661fc71cc9f9
Luiz de Milon <kori@openmailbox.org>
2015-09-20 01:33:29 -0400

    Add default case to operation switch

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 
 func main() {
 	switch os.Args[1] {
+	default:
+		fmt.Println("no operation specified")
 	case "build":
 		file := os.Args[2]
-		p := pkg.Prepare(file)
+		p := pkg.Build(file)
 		fmt.Println(p.Info.Name)
 	case "add":
 		fmt.Println("not implemented yet!")
