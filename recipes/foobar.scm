36a518f5978899da9ae9d9e3bc9b692131cacaa8
Luiz de Milon <luiz.de.milon@gmail.com>
2016-11-22 01:07:42 -0300

    correct sexprs recipe

--- recipes/foobar.scm
+++ recipes/foobar.scm
@@ -1,46 +1,47 @@
 (meta
   (name "foobar")
   (version "13.3.7")
-  (dependencies ("x" "y" "z"))
+  (dependencies '("x" "y" "z"))
   (replaces '("barfoo")))
 
 (source
   (download
-    "https://anexcellenturl.com/anexcellentprogram.tar.gz")
+  '("https://anexcellenturl.com/anexcellentprogram.tar.gz"
+    "https://anexcellenturl.com/anexcellentpatch.gz"))
   (local-files
-    "patch1.patch"
+  '("patch1.patch"
     "patch2.patch"
-    "patch3.patch")
+    "patch3.patch"))
   (sha256
-    "fooooooooooooooooooooooooooooooooooooooooooooooooooo"
+  '("fooooooooooooooooooooooooooooooooooooooooooooooooooo"
     "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar"
-    "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz"))
+    "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz")))
 
 (options
-  'strip-binaries
-  'static-binaries
-  'parallel-build)
+  '(strip-binaries
+    static-binaries
+    parallel-build))
 
 (features
   (define foo
-    ('(default true)
-     '(enable-flag "--enable-foo")
-     '(disable-flag "--disable-foo")
-     (requires
-       '(bar))))
+    '((default #t)
+      (enable-flag "--enable-foo")
+      (disable-flag "--disable-foo")
+      (requires
+        '())))
   (define bar
-    ('(default true)
-     '(enable-flag "--enable-bar")
-     '(disable-flag "--disable-bar")
-     (requires
-       '()))))
+    '((default #t)
+      (enable-flag "--enable-bar")
+      (disable-flag "--disable-bar")
+      (requires
+        '(bar)))))
 
 (extra
   (configure-stage
-     "--prefix=/usr"
-     "--execprefix=/usr")
+    '("--prefix=/usr"
+      "--execprefix=/usr"))
   (pre-build
-     "mkdir foo")
+     '("mkdir foo"))
   (post-build
-    "rm bar"))
+    '("rm bar")))
 
