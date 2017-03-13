54ca07284e28a4aed45ed6cde06aaeec0c0bb361
Luiz de Milon <luiz.de.milon@gmail.com>
2017-03-13 11:53:18 -0300

    correct sexprs recipe again

--- recipes/foobar.scm
+++ recipes/foobar.scm
@@ -1,47 +1,42 @@
-(meta
-  (name "foobar")
-  (version "13.3.7")
-  (dependencies '("x" "y" "z"))
-  (replaces '("barfoo")))
+(define meta
+  '((name "foobar")
+    (version "13.3.7")
+    (dependencies "x" "y" "z")
+    (replaces "barfoo")))
 
-(source
-  (download
-  '("https://anexcellenturl.com/anexcellentprogram.tar.gz"
-    "https://anexcellenturl.com/anexcellentpatch.gz"))
-  (local-files
-  '("patch1.patch"
-    "patch2.patch"
-    "patch3.patch"))
-  (sha256
-  '("fooooooooooooooooooooooooooooooooooooooooooooooooooo"
-    "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar"
-    "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz")))
+(define source
+  '((download
+       "https://anexcellenturl.com/anexcellentprogram.tar.gz"
+       "https://anexcellenturl.com/anexcellentpatch.gz")
+    (local-files
+      "patch1.patch"
+      "patch2.patch"
+      "patch3.patch")
+    (sha256
+      "fooooooooooooooooooooooooooooooooooooooooooooooooooo"
+      "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar"
+      "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz")))
 
-(options
+(define options
   '(strip-binaries
     static-binaries
     parallel-build))
 
-(features
-  (define foo
-    '((default #t)
-      (enable-flag "--enable-foo")
-      (disable-flag "--disable-foo")
-      (requires
-        '())))
-  (define bar
-    '((default #t)
-      (enable-flag "--enable-bar")
-      (disable-flag "--disable-bar")
-      (requires
-        '(bar)))))
+(define features
+  '((foo (default #t)
+          (enable-flag "--enable-foo")
+          (disable-flag "--disable-foo")
+          (requires (Nothing)))
+    (bar (default #t)
+         (enable-flag "--enable-bar")
+         (disable-flag "--disable-bar")
+         (requires foo))))
 
-(extra
-  (configure-stage
-    '("--prefix=/usr"
-      "--execprefix=/usr"))
-  (pre-build
-     '("mkdir foo"))
-  (post-build
-    '("rm bar")))
-
+(define extra
+  '((configure-stage
+       "--prefix=/usr"
+       "--execprefix=/usr")
+    (pre-build
+      "mkdir foo")
+    (post-build
+      "rm bar")))
